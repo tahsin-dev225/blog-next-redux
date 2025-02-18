950c39cf64c75dd8fdbc48cb7616a325445c11e0
tahsin-dev225 <ahmedtasin225@gmail.com>
2025-02-18 17:09:55 +0600

    quill dynamic

--- src/components/shared/CustomQuill.jsx
+++ src/components/shared/CustomQuill.jsx
@@ -1,8 +1,8 @@
 "use client"
 import dynamic from "next/dynamic";
 import 'react-quill-new/dist/quill.snow.css';
-import ReactQuill from 'react-quill-new';
-// const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+// import ReactQuill from 'react-quill-new';
+const ReactQuill = dynamic(() => import("react-quill-new"), { ssr: false });
 
 const CustomQuill = ({ value, onChange }) => {
   const modules = {
